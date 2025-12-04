8e5daebc83b66f759e410d8606a4d66993d48960
Ravindu Kaveesha <161436774+RK17904@users.noreply.github.com>
2025-12-04 14:48:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
-import helmet from 'helmet'; // <--- NEW SECURITY TOOL
+import helmet from 'helmet'; 
 
 const app = express();
 
